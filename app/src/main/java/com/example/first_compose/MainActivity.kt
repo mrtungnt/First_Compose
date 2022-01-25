c3467bd5bbe0d8799804a59fb13368c09fe4a7c3
tungnt <2tungnt@gmail.com>
2022-01-26 00:35:22 +0700

    lazy list

--- app/src/main/java/com/example/first_compose/MainActivity.kt
+++ app/src/main/java/com/example/first_compose/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.first_compose.ui.theme.First_ComposeTheme
@@ -37,12 +38,12 @@ fun App() {
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf("John", "Wick", "Parachute")) {
+fun Greetings(names: List<String> = List(1000) { "$it" }) {
     // A surface container using the 'background' color from the theme
     Surface(color = MaterialTheme.colors.background) {
-        Column(modifier = Modifier.padding(vertical = 4.dp)) {
-            for (name in names) {
-                Greeting(name = name)
+        LazyColumn {
+            items(items = names) {
+                Greeting(it)
             }
         }
     }
@@ -51,13 +52,16 @@ fun Greetings(names: List<String> = listOf("John", "Wick", "Parachute")) {
 @Composable
 fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Text(
-                text = "Hello $name!", modifier = Modifier.weight(1f)
+                text = "Hello $name!", modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             )
             OutlinedButton(onClick = { expanded.value = !expanded.value }) {
                 Text(
@@ -74,7 +78,7 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun OnboardingScreen(onclick: () -> Unit) {
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
     Surface() {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -84,7 +88,7 @@ fun OnboardingScreen(onclick: () -> Unit) {
             Text(text = "Welcome onboard")
             Button(
                 modifier = Modifier.padding(vertical = 10.dp),
-                onClick = onclick
+                onClick = onContinueClicked
             ) {
                 Text(text = "Continue")
             }
@@ -93,24 +97,16 @@ fun OnboardingScreen(onclick: () -> Unit) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true, widthDp = 320, heightDp = 350)
 @Composable
 fun DefaultPreview() {
-    App()
+    Greetings()
 }
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
-    First_ComposeTheme {
-        OnboardingScreen{}
-    }
-}
-
-@Preview(name = "Another Preview")
-@Composable
-fun Woo() {
-    First_ComposeTheme {
-        Text(text = "hello", color = Color.Magenta, modifier = Modifier.padding(22.dp))
+    First_ComposeTheme() {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
